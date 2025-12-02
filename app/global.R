17bfdfc13eb65a75816c13f2c63920bbfe938792
somin-park <sp2712@scarletmail.rutgers.edu>
2025-12-02 03:21:06 -0500

    changed data name

--- app/global.R
+++ app/global.R
@@ -1,5 +1,5 @@
 library(tidyverse)
 library(lubridate)
 
-ATUS_data <- read_csv("data/atus_activity_2020_24.csv", show_col_types = FALSE)
+activity_2020_24 <- read_csv("data/atus_activity_2020_24.csv", show_col_types = FALSE)
 meetup_events <- read_csv("data/meetup_art_events.csv", show_col_types = FALSE) %>% filter(!is.na(event_name) & event_name != "")
